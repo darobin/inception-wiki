d2efe878a3dcd0b1d490601a6dfd5c96df6ff7db
Robin Berjon <robin@berjon.com>
2011-11-05 12:19:28 -0700

    extra files

--- client/js/wiki.js
+++ client/js/wiki.js
@@ -1,1 +1,16 @@
-https://github.com/darobin/inception-wiki
+(function (exports, $) {
+    exports.wiki = {
+        root:   "/_rest/"
+    ,   get:    function (path) {
+        
+        }
+    ,   post:    function (path) {
+        
+        }
+    ,   del:    function (path) {
+        
+        }
+    };
+    
+    
+})(window, jQuery);
