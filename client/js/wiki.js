ec4ec31ea985b5285b0b8e610d94b325fb8d1c12
Robin Berjon <robin@berjon.com>
2011-11-05 12:26:58 -0700

    local api

--- client/js/wiki.js
+++ client/js/wiki.js
@@ -1,14 +1,34 @@
 (function (exports, $) {
     exports.wiki = {
         root:   "/_rest/"
-    ,   get:    function (path) {
-        
+    ,   online: true
+    ,   get:    function (path, cb) {
+            if (this.online) {
+                
+            }
+            else {
+                var cnt = localStorage.getItem(path);
+                if (cnt) cb(null, cnt);
+                else cb(404);
+            }
         }
-    ,   post:    function (path) {
-        
+    ,   post:    function (path, cnt, cb) {
+            localStorage.setItem(path, cnt);
+            if (this.online) {
+            
+            }
+            else {
+                cb(null);
+            }
         }
-    ,   del:    function (path) {
-        
+    ,   del:    function (path, cb) {
+            localStorage.deleteItem(path);
+            if (this.online) {
+                
+            }
+            else {
+                cb(null);
+            }
         }
     };
     
