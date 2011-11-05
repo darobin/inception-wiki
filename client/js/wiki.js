4e46c5661e2aa1ed30f0017f75999cd19c91e742
Robin Berjon <robin@berjon.com>
2011-11-05 12:54:12 -0700

    add dependencies

--- client/js/wiki.js
+++ client/js/wiki.js
@@ -1,52 +1,61 @@
 (function (exports, $) {
+    // JSONP client to Henri's server
+    exports.wiki = funtion (url) {
+        url = url || "http://tokyo.local:8080/_rest_/";
+        // XXX
+        //  make online depend on navigator.onLine
+        return {
+            root:   url
+        ,   online: true
+        ,   get:    function (path, cb) {
+                if (this.online) {
+                    $.ajax({
+                        url:        this.root + path
+                    ,   dataType:   "jsonp"
+                    ,   success:    function (data) { cb(null, data); }
+                    ,   error:      function (xhr, err) { cb(err); }
+                    });
+                }
+                else {
+                    var cnt = localStorage.getItem(path);
+                    if (cnt) cb(null, cnt);
+                    else cb(404);
+                }
+            }
+        ,   post:    function (path, cnt, cb) {
+                localStorage.setItem(path, cnt);
+                if (this.online) {
+                    $.ajax({
+                        url:        this.root + path
+                    ,   method:     "POST"
+                    ,   data:       cnt
+                    ,   dataType:   "jsonp"
+                    ,   success:    function () { cb(null); }
+                    ,   error:      function (xhr, err) { cb(err); }
+                    });
+                }
+                else {
+                    cb(null);
+                }
+            }
+        ,   del:    function (path, cb) {
+                localStorage.deleteItem(path);
+                if (this.online) {
+                    $.ajax({
+                        url:        this.root + path
+                    ,   method:     "DELETE"
+                    ,   dataType:   "jsonp"
+                    ,   success:    function () { cb(null); }
+                    ,   error:      function (xhr, err) { cb(err); }
+                    });
+                }
+                else {
+                    cb(null);
+                }
+            }
+        };
+    }
     exports.wiki = {
-        root:   "/_rest/"
-    ,   online: true
-    ,   get:    function (path, cb) {
-            if (this.online) {
-                $.ajax({
-                    url:        this.root + path
-                ,   dataType:   "html"
-                ,   success:    function (data) { cb(null, data); }
-                ,   error:      function (xhr, err) { cb(err); }
-                });
-            }
-            else {
-                var cnt = localStorage.getItem(path);
-                if (cnt) cb(null, cnt);
-                else cb(404);
-            }
-        }
-    ,   post:    function (path, cnt, cb) {
-            localStorage.setItem(path, cnt);
-            if (this.online) {
-                $.ajax({
-                    url:        this.root + path
-                ,   method:     "POST"
-                ,   data:       cnt
-                ,   dataType:   "html"
-                ,   success:    function () { cb(null); }
-                ,   error:      function (xhr, err) { cb(err); }
-                });
-            }
-            else {
-                cb(null);
-            }
-        }
-    ,   del:    function (path, cb) {
-            localStorage.deleteItem(path);
-            if (this.online) {
-                $.ajax({
-                    url:        this.root + path
-                ,   method:     "DELETE"
-                ,   success:    function () { cb(null); }
-                ,   error:      function (xhr, err) { cb(err); }
-                });
-            }
-            else {
-                cb(null);
-            }
-        }
     };
     
     
